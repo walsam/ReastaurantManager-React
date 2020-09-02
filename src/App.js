785d0b489ea134660dcaed77b8051da6c5c20b3c
Walid Maarad <walsam@hotmail.com>
2020-09-02 21:30:01 +0100

    the front end of the app managed by reactJS.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Home from './Home'
+import Managers from './Managers'
+
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                 <Container text style={{ marginTop: '7em' }}>
+                        <Route path="/" exact component={Home} />
+                     <Route path="/managers" exact component={Managers} />
+
+                 </Container>
+            </Router>
+        );
+    }
 }
 
-export default App;
+export default App
